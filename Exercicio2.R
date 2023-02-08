658824aba9540c3aef865bd7458ee07578b24018
SammyMar <samuel.martins7@hotmail.com>
2023-02-08 00:19:11 -0300

    .

--- Exercicio2.R
+++ Exercicio2.R
@@ -1,4 +1,5 @@
 ########## PACOTES ##############
+library(ggplot2)
 #################################
 #---------------------- QUESTAO 2 -----------------------------------
 set.seed(3666)
@@ -17,7 +18,6 @@ coefs <- c(.3,.1,-.5)
 n <- 100
 y <- AR(coefs,n)
 plot.ts(y)
-
 ### B #####################################################################
 
 #AMOSTRAL ------
@@ -41,14 +41,13 @@ lags <- c(0:(n-1))
 acfs <- data.frame(acf = acf.amostral(y),
                    lag = lags)
 #plot
-library(ggplot2)
 cline <- 1.96/sqrt(length(y))
 ggplot(data=acfs[1:50,], aes(x=lag, y=acf)) +
   geom_errorbar(aes(x=lag, ymax=acf, ymin=0), width=0) +
   geom_hline(aes(yintercept = 0))+
   geom_hline(aes(yintercept = cline), linetype = 2, color = 'darkblue') +
   geom_hline(aes(yintercept = -cline), linetype = 2, color = 'darkblue') +
-  ggtitle('Autocorrelação Amostral') + ylab('Autocorrelação') + xlab('Lags')
+   ylab('Autocorrelação') + xlab('Lags')
 #conferindo
 acf(y,lag.max = 50)
 # TEORICO ----
@@ -76,23 +75,23 @@ ggplot(data=acfs.t[1:50,], aes(x=lag, y=acf)) +
   geom_hline(aes(yintercept = 0))+
   geom_hline(aes(yintercept = cline), linetype = 2, color = 'darkblue') +
   geom_hline(aes(yintercept = -cline), linetype = 2, color = 'darkblue') +
-  ggtitle('Autocorrelação Teórica') + ylab('Autocorrelação') + xlab('Lags')
+  ylab('Autocorrelação') + xlab('Lags')
 pacf(y,lag.max = 50)
 
 ### C ######################################################################
+set.seed(234)
 logLik <- function(coefs, y) {
-  c1 <- coefs[1]
-  c2 <- coefs[2]
-  c3 <- coefs[3]
-  sigma2 <- coefs[4]
+  a1 <- coefs[1]
+  a2 <- coefs[2]
+  a3 <- coefs[3]
+  c <- coefs[4]
   n <- length(y)
-  aux <- -n/2*log(2*pi*sigma2)
-  loglik <- 0
-  for (t in 4:n) {
-    loglik <-loglik + (y[t] - c1 * y[t-1] - c2 * y[t-2] - c3 * y[t-3])^2
+  aux <- 1/(n - 3)
+  sigma2 <- 0
+  for(t in 4:n){
+    sigma2 <- sigma2 + (y[t] - c - y[t-1]*a1 - y[t-2]*a2 - y[t-3]*a3)^2
   }
-  loglik <- aux - loglik/(2*sigma2)
-  return(-loglik)
+  return(sigma2*aux)
 }
 
 # Define os parâmetros iniciais
@@ -102,16 +101,18 @@ y <- AR(c(.3,.1,-.5),10000)
 fit <- optim(coefs_iniciais, logLik, method = "BFGS", y = y)
 
 # Exibe os parâmetros otimizados
-fit$par
-
-
+fit$par ## a1,a2,a3 e c
+fit$value # sigma^2
+# set.seed(43); y <- arima.sim(n = 100, list(ar = c(0.3, 0.1, -0.5)))
+# fit <- optim(coefs_iniciais, logLik, method = "BFGS", y = y)
 ### D #################################################################
+set.seed(27)
 B <- 2000
 a1 <- vector()
 a2 <- vector()
 a3 <- vector()
 sigma2 <- vector()
-mean.y <- vector()
+c <- vector()
 n <- 500
 coefs <- c(.3,.1,-.5)
 for(b in 1:B){
@@ -120,12 +121,18 @@ for(b in 1:B){
   a1[b] <- fit$par[1]
   a2[b] <- fit$par[2]
   a3[b] <- fit$par[3]
-  sigma2[b]<- fit$par[4]
+  c[b]<- fit$par[4]
+  sigma2 <- fit$value
 
 }
 estimativas <- data.frame(a1 = a1,
                           a2 = a2,
                           a3 = a3,
+                          c = c,
                           sigma2 = sigma2)
-apply(estimativas,2,mean )
-apply(estimativas,2,var)
+apply(estimativas,2,mean ) # MEDIA DOS PARAMETROS
+apply(estimativas[,1:4],2,shapiro.test) #TESTE DE NORMALIDADE DE SHAPIRO WILK
+qqnorm(estimativas$a1)
+qqnorm(estimativas$a2)
+qqnorm(estimativas$a3)
+qqnorm(estimativas$c)
